ad18eb425818f74ea5a126edf11888cd1801ae85
euijin1212 <jck0369@naver.com>
2025-01-11 21:06:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -4,6 +4,24 @@ using namespace std;
 
 int n,m,ans,x;
 
+
+void fx(int k,int g){
+    if(k == g){
+        ans++;
+        return;
+    }
+    else if(k > g)
+    return;
+
+    fx(k+1,g);
+    fx(k+2,g);
+    fx(k+3,g);
+
+    return;
+}
+
+
+
 int main(){
     
     cin  >> x;
@@ -11,53 +29,12 @@ int main(){
     for(int i = 0; i < x; i++){
         cin >> n;
 
-    for(int i = 1; i < 4; i++){
-            if( i == n){
-                ans++;
-            }
-                    for(int j = 1; j < 4; j++){
-            if( i+j == n)
-                ans++;
-        
-                            for(int k = 1; k < 4; k++){
-            if( i+j+k == n)
-                ans++;
-        
-                            for(int l = 1; l < 4; l++){
-            if( i+j+k+l == n)
-                ans++;
-        
-                            for(int q = 1; q < 4; q++){
-            if(i+j+k+l+q == n)
-                ans++;
-        
-                            for(int w = 1; w < 4; w++){
-            if( i+j+k+l+q+w == n)
-                ans++;
-        
-                            for(int e = 1; e < 4; e++){
-            if(i+j+k+l+q+w+ e == n)
-                ans++;
-        
-                            for(int r = 1; r < 4; r++){
-            if( r+i+j+k+l+q+w+ e == n)
-                ans++;
-        
-                            for(int t = 1; t < 4; t++){
-            if(r+i+j+k+l+q+w+e+t == n)
-                ans++;
-        
-                            for(int y = 1; y < 4; y++){
-            if( y+r+i+j+k+l+q+w+e+t == n)
-                ans++;
-        
-                            for(int u = 1; u < 4; u++){
-            if( u+y+r+i+j+k+l+q+w+e+t == n)
-                ans++;
-        }}}}}}}}}}}
 
-    cout << ans << '\n';
-    ans = 0;
+
+        fx(0,n);
+
+        cout << ans << '\n';
+        ans = 0;
     }
     return 0;
 }
