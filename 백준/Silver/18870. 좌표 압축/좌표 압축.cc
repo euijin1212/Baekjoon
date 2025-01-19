823f9f678750fae2122d2cf67ff4e53077002622
euijin1212 <jck0369@naver.com>
2025-01-19 17:35:40 +0900

    [Silver II] Title: 좌표 압축, Time: 884 ms, Memory: 15988 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,38 +1,58 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n,x;
+int n;
 
+int arr[1000001];
+int carr[1000001];
 vector <int> vt;
-vector <int> ans;
-vector <int> v;
+
+int fx(int k){     // low_bound 구현
+
+    int st = 0;
+    int en = vt.size();
+    int mid = 0;
+    while(st < en){
+
+        mid = (st+en)/2;
+
+        if(vt[mid] < k)
+            st = mid + 1;
+        else 
+            en = mid;
+    }
+
+    return st;
+
+}
 
 
-
-int main()
-{
-
+int main(){
+    
     cin >> n;
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
+        int temp = 0;
+        cin >> temp;
+        arr[i] = temp;
+        carr[i] = temp;
+    }
+    sort(carr, carr + n);
+    
+    for(int i = 1 ; i < n; i++){  // 중복 제거
 
-        cin >> x;
+        if(carr[i-1] != carr[i])
+            vt.push_back(carr[i-1]);
 
-        vt.push_back(x);
-        ans.push_back(x);
-        
+
     }
 
-    sort(vt.begin(),vt.end());
 
-    for(int i = 0; i < n; i++){
-
-        if(i ==0 || vt[i-1] != vt[i])
-            v.push_back(vt[i]);
-        
-    }
-    for(int i = 0; i < n; i++){
-        cout << lower_bound(v.begin(),v.end(),ans[i]) - v.begin() << ' ';
+    for (int i = 0; i < n; i++)
+    {
+        cout << fx(arr[i]) << ' ';
     }
 
     return 0;
